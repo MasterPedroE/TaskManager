fb3b2f173a89c6a83b4e5e3dd6aeee5a89732a2b
Pedro <flaviolorear@gmail.com>
2025-02-26 23:41:54 -0300

    testemodelok!

--- usuario/tests.py
+++ usuario/tests.py
@@ -1,1 +1,19 @@
 from django.test import TestCase
+from .models import Usuario
+
+class UsuarioTestModel(TestCase):
+    def test_criar_usuario(self):
+        usuario = Usuario.objects.create_user(
+            username='testuser',
+            email='test@example.com',
+            password='123123',
+            nome='Test User'
+        )
+
+        self.assertEqual(usuario.email, 'test@example.com')
+
+        self.assertEqual(usuario.username, 'testuser')
+
+        self.assertEqual(usuario.nome, 'Test User')
+
+        self.assertTrue(usuario.check_password('123123'))
